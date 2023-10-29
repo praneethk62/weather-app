e3da9d34d2bc4ca9f1328e2db4ab01f54e432849
praneeth k <kpraneeth62@gmail.com>
2023-10-29 13:52:14 +0530

    weather app complete

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Weatherapp from './Components/Weather app/Weatherapp';
+
+function App() {
+  return (
+    <div className="App">
+    <Weatherapp/>
+    </div>
+  );
+}
+
+export default App;
